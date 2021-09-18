c084a86da3e9c6e4b7242c4a1d849027019cbe9e
Nadrixa <adrianmunozgamez@gmail.com>
2021-09-18 14:45:01 +0200

    test: updated e2e test

--- test/retrieveBeltPoints.e2e.ts
+++ test/retrieveBeltPoints.e2e.ts
@@ -5,9 +5,10 @@ describe('Retrieve belt points', () => {
     it('Should return points to get yellow belt', async () => {
         const beltColor = 'yellow';
 
-        const { data: points } = (await axios.get(`http://localhost:3000/dev/belts/${beltColor}/points`));
+        const { data: points, status } = (await axios.get(`http://localhost:3000/dev/belts/${beltColor}/points`));
 
-        expect(points).toBe(500);
+        expect(status).toBe(200);
+        expect(points).toBe(100);
     });
 
 });
